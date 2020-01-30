2cd8f3da7e89cfa31f9d68f9c697f72e3ef25505
Salman Akhtar <TheLeopards@protonmail.com>
2020-01-30 22:24:44 +0500

    addition of sbox

--- aes.h
+++ aes.h
@@ -35,9 +35,9 @@
 #define AES_K_192 1
 #define AES_K_256 1
 
-//one thing is same for AES256,192 and 128 which  is block size for ecryption
-#define AES_BLOCK_SIZE 128/8 // 16 Bytes while in DES it was 64 bits
-
+//Same for AES256,192 and 128 which  is block size for ecryption
+#define AES_BLOCK_SIZE 128/8 // 16 Bytes while in DES it was 64 bits(8 bytes)
+#define NmColumns 4 //4x4 box
 //Note : Rijndael's key expansion.
 
         //Expands an 128,192,256 key into an 176,208,240 bytes key
@@ -49,18 +49,19 @@
     #define AES_KEY_LENGTH 128/8 //16 Bytes key
     #define AES_EXPANDED_KEY 176 //176 bytes expanded key
     #define AES_ROUNDS 10 //10 rounds for 128 bits key
-
+    #define NM32W 128/32 //4 number of 32-bit words
 // parameters for AES_K_192
 #elif defined(AES_K_192) && (AES_K_192==1)
     #define AES_KEY_LENGTH 192/8 //24 Bytes key
     #define AES_EXPANDED_KEY 208 //208 bytes expanded key
     #define AES_ROUNDS 12 //12 rounds for 192 bits key
-
+    #define NM32W 192/32 //6 number of 32-bit words
 // parameters for AES_K_256
 #else
     #define AES_KEY_LENGTH 256/8 //32 Bytes key
     #define AES_EXPANDED_KEY 240 //240 bytes expanded key
     #define AES_ROUNDS 14 //14 rounds for 256 bits key
+    #define NM32W 256/32 //4 number of 32-bit words
 #endif
 
 
